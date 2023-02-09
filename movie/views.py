45bb2ccd2d8e5cd6805dc48e2f54a8139cfb01b9
JoseDavid10190307 <jdvalenci2@eafit.edu.co>
2023-02-09 11:56:33 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -7,4 +7,4 @@ def home(request):
     
     #return HttpResponse('<h1>Welcome to Home Page </h1>')
     #return render (request, 'home.html')
-    return render (request, 'home.html', {'name': 'Greg Lim'})
+    return render (request, 'home.html', {'name': 'Jose David Valencia'})
